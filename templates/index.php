51a7f9e5584551aacb8060b5ae48cc4e2f6f92c1
carlos alva <carlos.alva.sandoval@gmail.com>
2017-09-06 21:15:27 -0500

    mejora look and feel

--- templates/index.php
+++ templates/index.php
@@ -34,50 +34,33 @@
       </div>
       <div class="col-md-10">
         <form class="" method="get" id="download" action="getvideo.php">
+          <div class="text-center">
+            <h1 class="form-download-heading">
+              <img src="img/descargarvideoyoutube_youtube_logo.png" alt="Descargar videos de youtube" class="img-responsive" style="display: inline"/> Descargar videos de Youtube</h1>
+            <p class="help-block">
+              Está herramienta te permite descargar videos de youtube convirtiéndolos a múltiples formatos:
+              <br> <span class="label label-default">mp4</span> ,<span class="label label-default">mp3</span>, <span class="label label-default">3gpp</span> entre otros.
+            </p>
+          </div>
+          <div class="panel panel-default" style="border: 3px dashed red">
 
-          <div class="row">
-            <div class="col-md-8">
-              <h1 class="form-download-heading">
-                <img src="img/descargarvideoyoutube_youtube_logo.png" alt="Descargar videos de youtube" class="img-responsive" style="display: inline"/> Descargar videos de Youtube</h1>
-
-              <p class="help-block">
-                Está herramienta te permite descargar videos de youtube convirtiéndolos a múltiples formatos:
-                <br> <span class="label label-default">mp4</span> ,<span class="label label-default">mp3</span>, <span class="label label-default">3gpp</span> entre otros.
-              </p>
-
+            <div class="panel-body">
               <div class="video-info">
-                <h3>Pasos para descargar videos o audios de youtube:</h3>
+                <h4>Pasos para descargar videos o audios de youtube:</h4>
                 <ol class="h5">
                   <li>Copiar URL desde youtube del video que deseas descargarte</li>
                   <li>Pegar el enlace en la casilla de descarga y presionar botón "descargar"</li>
                   <li>Finalmente elige el formato que desees y aprieta el botón descargar</li>
                 </ol>
               </div>
-            </div>
-            <div class="col-md-4 hidden-xs">
-              <script type="text/javascript">
-          (function () {
-            if (window.CHITIKA === undefined) {
-              window.CHITIKA = {'units': []};
-            }
-            ;
-            var unit = {"calltype": "async[2]", "publisher": "carlosalvasandoval", "width": 300, "height": 250, "sid": "Chitika Default"};
-            var placement_id = window.CHITIKA.units.length;
-            window.CHITIKA.units.push(unit);
-            document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
-          }());
-              </script>
-              <script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
+              <div class="input-group">
+                <input type="text" name="videoid" id="videoid" class="form-control" placeholder="Pega el enlace de youtube : https://www.youtube.com/watch?v=...">
+                <span class="input-group-btn">
+                  <input class="btn btn-danger" type="submit" name="type" id="type" value="Descargar" />
+                </span>
+              </div>
             </div>
           </div>
-          <div class="input-group well">
-            <input type="text" name="videoid" id="videoid" class="form-control input-lg" placeholder="Pega aquí el enlace de youtube : https://www.youtube.com/watch?v=...">
-            <span class="input-group-btn">
-              <input class="btn btn-danger btn-lg" type="submit" name="type" id="type" value="Descargar" />
-            </span>
-          </div>
-
-
           <div class="visible-xs-block">
             <script type="text/javascript">
           (function () {
@@ -95,6 +78,7 @@
           </div>
 
 
+
           <div class="hidden-xs">
             <script type="text/javascript">
           (function () {
@@ -103,6 +87,21 @@
             }
             ;
             var unit = {"calltype": "async[2]", "publisher": "carlosalvasandoval", "width": 728, "height": 90, "sid": "Chitika Default"};
+            var placement_id = window.CHITIKA.units.length;
+            window.CHITIKA.units.push(unit);
+            document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
+          }());
+            </script>
+            <script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
+          </div>
+          <div class="hidden-xs">
+            <script type="text/javascript">
+          (function () {
+            if (window.CHITIKA === undefined) {
+              window.CHITIKA = {'units': []};
+            }
+            ;
+            var unit = {"calltype": "async[2]", "publisher": "carlosalvasandoval", "width": 300, "height": 250, "sid": "Chitika Default"};
             var placement_id = window.CHITIKA.units.length;
             window.CHITIKA.units.push(unit);
             document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
