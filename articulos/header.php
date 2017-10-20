ccc79b9e07ff1bc43827c804d594c86999b0f97f
carlos alva <carlos.alva.sandoval@gmail.com>
2017-10-20 10:40:04 -0500

    marcado facebook article

--- articulos/header.php
+++ articulos/header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="Cache-control" content="public">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <meta property="og:url"                content="https://descargarvideoyoutube.com" />
-    <meta property="og:type"               content="website" />
+    <meta property="og:type"               content="article" />
     <meta property="og:title"              content="DescargarVideoYotube.com | <?php echo $title ?>" />
     <meta property="og:description"        content="<?php echo $description ?>" />
     <meta property="og:image"              content="<?php echo $img?>" />
